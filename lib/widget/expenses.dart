245432102832d5fce05d74b5951875390fde3a69
Han-Websterr <han.o828.m104@gmail.com>
2024-07-28 10:50:01 +0900

    Add app bar and cards

---
+++ lib/widget/expenses.dart
@@ -0,0 +1,53 @@
+import 'package:expense_tracker/widget/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpense = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 20.00,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Groceries',
+        amount: 50.00,
+        date: DateTime.now(),
+        category: Category.food
+        ),
+  ];
+
+  void _openAddExpenseForm() {
+    showModalBottomSheet(context: context, builder: (ctx) => const Text('Add Expense Form'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseForm, 
+            icon: const Icon(Icons.add),
+            ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('Charts'),
+          Expanded(child: ExpenseList(expenses: _registeredExpense)),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widget/new_expense.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() {
+    return _NewExpenseState();
+  }
+
+  class _NewExpenseState extends State<NewExpense> {
+
+  }
+}
