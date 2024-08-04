5ed1e8158d57612bbfa92ae4fd2a04258e30e783
Han-Websterr <han.o828.m104@gmail.com>
2024-08-04 14:25:57 +0900

    Add title and amount

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -10,27 +10,59 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
       child: Card(
         child: Padding(
-          padding: EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
               TextField(
+                controller: _titleController,
                 maxLength: 50,
-                decoration: InputDecoration(
-                  label: Text('title'),
+                decoration: const InputDecoration(
+                  label: Text('Title'),
                 ),
-              )
+              ),
+              TextField(
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  prefixText: "\$",
+                  label: Text('Amount'),
+                ),
+              ),
+              Row(
+                children: [
+                  TextButton(onPressed: () {
+                    Navigator.pop(context);
+                  }, child: const Text('cancell')),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {
+                      print(_titleController.text);
+                      print(_amountController.text);
+                    },
+                    child: const Text('Add Expense'),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
       ),
     );
-
   }
 }
