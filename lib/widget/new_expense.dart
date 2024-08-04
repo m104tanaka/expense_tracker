e23044185cde2dc203e8fb21b2fd7afdcdd66ec5
Han-Websterr <han.o828.m104@gmail.com>
2024-08-04 10:36:30 +0900

    Add new expense

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -7,8 +7,21 @@ class NewExpense extends StatefulWidget {
   State<NewExpense> createState() {
     return _NewExpenseState();
   }
+}
 
-  class _NewExpenseState extends State<NewExpense> {
+class _NewExpenseState extends State<NewExpense> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text('New Expense'),
+        ),
+      ),
+    );
 
   }
 }
